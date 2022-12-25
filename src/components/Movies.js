447ef3d5e4aeb54ec319f2a42ac26ea5f6e3ba38
Anatolii Panchenko <michael_frost_ng@gmail.com>
2022-12-26 01:27:41 +0200

    add recommended

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,42 +1,45 @@
 import React from "react";
 import styled from "styled-components";
 
-function Viewers() {
+function Movies() {
   return (
     <Container>
-      <Wrap>
-        <img src="/images/viewers-disney.png" />
-      </Wrap>
-      <Wrap>
-        <img src="/images/viewers-marvel.png" />
-      </Wrap>
-      <Wrap>
-        <img src="/images/viewers-national.png" />
-      </Wrap>
-      <Wrap>
-        <img src="/images/viewers-pixar.png" />
-      </Wrap>
-      <Wrap>
-        <img src="/images/viewers-starwars.png" />
-      </Wrap>
+      <h4>Recommended for You</h4>
+      <Content>
+        <Wrap>
+          <img src="/images/zim-cartoon.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/ginger-cartoon.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/fairy-cartoon.jpg" />
+        </Wrap>
+        <Wrap>
+          <img src="/images/avatar-cartoon.jpg" />
+        </Wrap>
+      </Content>
     </Container>
   );
 }
 
-export default Viewers;
+export default Movies;
 
 const Container = styled.div`
-  margin-top: 30px;
+  padding: 0 26px;
+`;
+
+const Content = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
   gap: 25px;
-  padding: 26px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
 `;
 
 const Wrap = styled.div`
   cursor: pointer;
   border: 3px solid rgba(249, 249, 249, 0.1);
   border-radius: 10px;
+  overflow: hidden;
   box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px -10px,
     rgba(0 0 0 / 73%) 0px 16px 10px -10px;
   transition: all 0.25s linear 0s;
