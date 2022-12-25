01504d86e6c714d845316bed475b26cfdda7df68
Anatolii Panchenko <michael_frost_ng@gmail.com>
2022-12-25 11:31:21 +0200

    add viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,7 +1,56 @@
 import React from "react";
+import styled from "styled-components";
 
 function Viewers() {
-  return <div>Viewers</div>;
+  return (
+    <Container>
+      <Wrap>
+        <img src="/images/viewers-disney.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" />
+      </Wrap>
+    </Container>
+  );
 }
 
 export default Viewers;
+
+const Container = styled.div`
+  margin-top: 30px;
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  gap: 25px;
+  padding: 30px 0 26px 0;
+`;
+
+const Wrap = styled.div`
+  cursor: pointer;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  border-radius: 10px;
+  box-shadow: rgba(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgba(0 0 0 / 73%) 0px 16px 10px -10px;
+  transition: all 0.25s linear 0s;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    box-shadow: rgba(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgba(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(1.05);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
